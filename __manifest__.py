fef294af443143217926abc15e8f83c915ca440c
MauroAntonioBogado1990 <mauroantoniobogado@gmail.com>
2025-12-03 17:25:25 -0300

    add new values in reporte remito

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+
+{
+    'name': 'Exe reporte para impresora',
+    'version': '17.8.0',
+    'depends': [
+        'base','sale','stock','product', 'account',
+    ],
+    'author': 'Mauro Bogado, Exemax',
+    'website': '"http://www.exemax.com.ar"',
+    'summary': 'Modulo para reporte de remitos para impresora',
+    'category': 'Extra Tools',
+    'description': '''
+   'Modulo para remitos'.
+    ''',
+    'data': [
+       'report/reporte_impresora.xml',
+        
+       
+    ],
+}
